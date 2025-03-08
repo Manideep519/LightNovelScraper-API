66b734266dd93250ca1620748b391da022b64b8c
Manideep519 <manideepyadav359@gmail.com>
2025-03-08 17:22:38 +0530

    feat: Update baseScrapeURL to use new domain

--- routes.js
+++ routes.js
@@ -5,10 +5,12 @@ const cheerio = require("cheerio");
 const middlewares = require("./middlewares");
 const options = middlewares.options;
 
-const baseScrapeURL = "https://boxnovel.com/";
+const baseScrapeURL = "https://novgo.co/";
 
 router.get("/", (req, res, next) => {
-  res.send("API is up and runing \n\n Navigate using below link \n\n add the following to your url /novel-list/1/views ");
+  res.send(
+    "API is up and runing \n\n Navigate using below link \n\n add the following to your url /novel-list/1/views "
+  );
 });
 
 router.get("/novel/:name", (req, res, next) => {
@@ -19,7 +21,14 @@ router.get("/novel/:name", (req, res, next) => {
   (async () => {
     let novelData = {};
     try {
-      const browser = await puppeteer.launch({ args: [`--proxy-server =${options.proxy}:${options.port}`, `--ignore-certificate-errors`, `--no-sandbox`, "--disable-extensions"] });
+      const browser = await puppeteer.launch({
+        args: [
+          `--proxy-server =${options.proxy}:${options.port}`,
+          `--ignore-certificate-errors`,
+          `--no-sandbox`,
+          "--disable-extensions",
+        ],
+      });
       const page = await browser.newPage();
       page.setUserAgent(options.useAgent);
       await page.setRequestInterception(true);
@@ -31,11 +40,15 @@ router.get("/novel/:name", (req, res, next) => {
         }
       });
       await page.goto(baseScrapeURL.concat(encodedURI));
-      await page.waitForSelector("#manga-chapters-holder > div.page-content-listing.single-page > div > ul");
+      await page.waitForSelector(
+        "#manga-chapters-holder > div.page-content-listing.single-page > div > ul"
+      );
       const content = await page.content();
       const $ = cheerio.load(content);
       novelData.name = $("div.post-title h1").html().trim();
-      novelData.imageUrl = $("div.profile-manga.summary-layout-1 > div > div > div > div.tab-summary > div.summary_image > a > img").attr("src");
+      novelData.imageUrl = $(
+        "div.profile-manga.summary-layout-1 > div > div > div > div.tab-summary > div.summary_image > a > img"
+      ).attr("src");
       novelData.author = $(" div.summary-content > div.author-content > a").html();
       novelData.rating = $("div.post-total-rating span.score").html();
       novelData.status = $("div.post-status div.summary-content:last-child").html().trim();
@@ -47,15 +60,23 @@ router.get("/novel/:name", (req, res, next) => {
         .each((i, elm) => {
           novelData.geners[i] = elm.data;
         });
-      $("#manga-chapters-holder > div.page-content-listing.single-page > div > ul li.wp-manga-chapter a").each((i, elm) => {
+      $(
+        "#manga-chapters-holder > div.page-content-listing.single-page > div > ul li.wp-manga-chapter a"
+      ).each((i, elm) => {
         elm.children.forEach((node) => {
           novelData.chaptersListData.push({ chapterName: node.data.trim() });
         });
       });
-      $("#manga-chapters-holder > div.page-content-listing.single-page > div > ul li.wp-manga-chapter a").each((i, elm) => {
-        novelData.chaptersListData[i].chapterLinkName = elm.attribs.href.replace(`${baseScrapeURL}`, `${protocol}://${url}/`).slice(0, -1);
+      $(
+        "#manga-chapters-holder > div.page-content-listing.single-page > div > ul li.wp-manga-chapter a"
+      ).each((i, elm) => {
+        novelData.chaptersListData[i].chapterLinkName = elm.attribs.href
+          .replace(`${baseScrapeURL}`, `${protocol}://${url}/`)
+          .slice(0, -1);
       });
-      $("#manga-chapters-holder > div.page-content-listing.single-page > div > ul li.wp-manga-chapter span.chapter-release-date i")
+      $(
+        "#manga-chapters-holder > div.page-content-listing.single-page > div > ul li.wp-manga-chapter span.chapter-release-date i"
+      )
         .contents()
         .each((i, elm) => {
           novelData.chaptersListData[i].chapterReleaseTime = elm.data;
@@ -89,7 +110,14 @@ router.get("/novel/:name/:chapter", (req, res, next) => {
   };
   (async () => {
     try {
-      const browser = await puppeteer.launch({ args: [`--proxy-server =${options.proxy}:${options.port}`, `--ignore-certificate-errors`, `--no-sandbox`, "--disable-extensions"] });
+      const browser = await puppeteer.launch({
+        args: [
+          `--proxy-server =${options.proxy}:${options.port}`,
+          `--ignore-certificate-errors`,
+          `--no-sandbox`,
+          "--disable-extensions",
+        ],
+      });
       const page = await browser.newPage();
       page.setUserAgent(options.useAgent);
       await page.setRequestInterception(true);
@@ -129,7 +157,14 @@ router.get("/novel-list/:page/:order", (req, res, next) => {
   let encodedURI = encodeURI(`/novel/page/${page}/?m_orderby=${viewBy}`);
   (async () => {
     try {
-      const browser = await puppeteer.launch({ args: [`--proxy-server =${options.proxy}:${options.port}`, `--ignore-certificate-errors`, `--no-sandbox`, "--disable-extensions"] });
+      const browser = await puppeteer.launch({
+        args: [
+          `--proxy-server =${options.proxy}:${options.port}`,
+          `--ignore-certificate-errors`,
+          `--no-sandbox`,
+          "--disable-extensions",
+        ],
+      });
       const page = await browser.newPage();
       page.setUserAgent(options.useAgent);
       await page.setRequestInterception(true);
@@ -147,7 +182,10 @@ router.get("/novel-list/:page/:order", (req, res, next) => {
       $("div.item-summary > div.post-title.font-title > h3 > a")
         .contents()
         .each((i, elm) => {
-          novel.push({ title: elm.data, link: elm.parentNode.attribs.href.replace(`${baseScrapeURL}`, `${protocol}://${url}/`) });
+          novel.push({
+            title: elm.data,
+            link: elm.parentNode.attribs.href.replace(`${baseScrapeURL}`, `${protocol}://${url}/`),
+          });
         });
 
       $("div.page-item-detail a img").each(async (i, elm) => {
@@ -178,7 +216,9 @@ router.get("/novel-list/:page/:order", (req, res, next) => {
       res.end();
     } catch (error) {
       console.log("Error: something went wrong in getNovel List, or check the url" + error);
-      res.send(JSON.stringify("Error: something went wrong in getNovel List, or check the url", error));
+      res.send(
+        JSON.stringify("Error: something went wrong in getNovel List, or check the url", error)
+      );
       res.end();
     }
   })();
